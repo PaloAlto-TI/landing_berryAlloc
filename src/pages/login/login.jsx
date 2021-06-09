4de548f4138f40172968594aad4f3061e24b3923
unknown <jcubero@paloalto.com.ec>
2021-06-09 16:08:54 -0500

    Login + validacion de sesion

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { GoogleLogin } from 'react-google-login';
 //import UsuarioService from '../../services/Usuarios/UsuariosService';
-import { useState } from 'react';
-import {usuarios} from "../../utils/Usuarios";
+// import { useState } from 'react';
+import { usuarios } from "../../utils/Usuarios";
 
 
 import "./login.css";
 import { useHistory } from "react-router";
-
-
-
-
-
-
 
 const Login = () => {
   let history = useHistory();
@@ -21,32 +15,35 @@ const Login = () => {
     history.push("/home");
   }
 
-    const responseGoogle = (response) => {
-      let usuarioCheck=  usuarios.find((u) => u.correo === response.profileObj.email);
 
-      if(usuarioCheck)
-      {
-        console.log(response.profileObj.email);
-        localStorage.setItem('user', JSON.stringify(usuarioCheck));
-        handleClick();
-      }
-      else{
-        console.log("no encontro");   
-      }
-      }
-    
-//hola
-return(<div>
-<GoogleLogin
-                clientId="610747110294-o2cruk32qs9j2mi3o9ob1b7dpu4ib35u.apps.googleusercontent.com"
-                buttonText="Google"
-                onSuccess={responseGoogle}
+  const responseGoogle = (response) => {
+    let usuarioCheck = usuarios.find((u) => u.correo === response.profileObj.email);
 
-                onFailure={responseGoogle}
-                cookiePolicy={"single_host_origin"}
-              />
+    if (usuarioCheck) {
+      console.log(response.profileObj.email);
+      localStorage.setItem('user', JSON.stringify(usuarioCheck));
+      handleClick();
+    }
+    else {
+      console.log("no encontro");
+    }
+  }
 
-</div>);
+  //hola
+  return (<div>
+    {
+      localStorage.getItem("user")===null ?
+        <GoogleLogin
+          clientId="610747110294-o2cruk32qs9j2mi3o9ob1b7dpu4ib35u.apps.googleusercontent.com"
+          buttonText="Google"
+          onSuccess={responseGoogle}
+
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />: handleClick()
+    }
+
+  </div>);
 }
 //"holaaa"
 export default Login;
