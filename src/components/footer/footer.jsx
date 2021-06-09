15996b276b526a9053eb5268381f8c750c7ef228
mcoronel2021pa <mcoronel@paloalto.com.ec>
2021-06-09 10:30:38 -0500

    FOOTER ESTRUCTURA - MC

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import "./footer.css";
-import { PageHeader, Button } from "antd";
+// import { PageHeader, Button } from "antd";
 // import { PageHeader, Tag, Button, Statistic, Descriptions, Row } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, HeartOutlined } from '@ant-design/icons';
+import { Row, Col } from 'antd';
 
 const Footer = () => {
     return (
         <footer className="main-footer">
-            <div>hoa</div>
+            <Row>
+                <Col span={8} className="labels-footer">Palo Alto - {(new Date().getFullYear())}</Col>
+                <Col span={8} className="labels-footer">With <HeartOutlined spin="true" style={{ fontSize: '16px', color: 'rgb(255 0 0)' }}/> TI</Col>
+                <Col span={8} className="labels-footer"><p>&copy; 2021 PA - TI | All Rights Reserved. <UserOutlined className="icons-footer"/></p></Col>
+            </Row>
         </footer>
     )
 }
