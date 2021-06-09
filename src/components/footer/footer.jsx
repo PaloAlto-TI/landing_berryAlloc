235104041343b806d89b8ff5f57a4ed984093273
mcoronel2021pa <mcoronel@paloalto.com.ec>
2021-06-09 14:50:11 -0500

    FOOTER PRUEBAS - MC

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
     return (
         <footer className="main-footer">
             <Row>
-                <Col span={8} className="labels-footer">Palo Alto - {(new Date().getFullYear())}</Col>
-                <Col span={8} className="labels-footer">With <HeartOutlined spin="true" style={{ fontSize: '16px', color: 'rgb(255 0 0)' }}/> TI</Col>
+                <Col span={8} className="labels-footer" style={{ alignSelf: 'end' }}>Palo Alto - {(new Date().getFullYear())}</Col>
+                <Col span={8} className="labels-footer"><a href={'https://www.youtube.com/watch?v=ZZmW0iiRGAs'} target="_blank" rel="noreferrer">With <HeartOutlined spin="true" style={{ fontSize: '16px', color: 'rgb(255 0 0)' }}/> TI</a></Col>
                 <Col span={8} className="labels-footer"><p>&copy; 2021 PA - TI | All Rights Reserved. <UserOutlined className="icons-footer"/></p></Col>
             </Row>
         </footer>
