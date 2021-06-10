a2fdbe29ae98e87af8116858c59890a63dca88ed
mcoronel2021pa <mcoronel@paloalto.com.ec>
2021-06-10 08:16:28 -0500

    CAMBIOS  - MC

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
     return (
         <footer className="main-footer">
             <Row>
-                <Col span={8} className="labels-footer" style={{ alignSelf: 'end' }}>Palo Alto - {(new Date().getFullYear())}</Col>
-                <Col span={8} className="labels-footer"><a href={'https://www.youtube.com/watch?v=ZZmW0iiRGAs'} target="_blank" rel="noreferrer">With <HeartOutlined spin="true" style={{ fontSize: '16px', color: 'rgb(255 0 0)' }}/> TI</a></Col>
-                <Col span={8} className="labels-footer"><p>&copy; 2021 PA - TI | All Rights Reserved. <UserOutlined className="icons-footer"/></p></Col>
+                <Col span={6} className="labels-footer" style={{ alignSelf: 'end' }}>Palo Alto - {(new Date().getFullYear())}</Col>
+                <Col span={12} className="labels-footer">With <a href={'https://www.youtube.com/watch?v=ZZmW0iiRGAs'} target="_blank" rel="noreferrer"><HeartOutlined spin="true" style={{ fontSize: '16px', color: 'rgb(255 0 0)' }}/></a> TI</Col>
+                <Col span={6} className="labels-footer"><p>&copy; 2021 PA - TI | All Rights Reserved. <UserOutlined className="icons-footer"/></p></Col>
             </Row>
         </footer>
     )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,31 +1,42 @@
 import React from "react";
-import { PageHeader, Button } from "antd";
+import { Menu, Button, Dropdown } from "antd";
 // import { PageHeader, Tag, Button, Statistic, Descriptions, Row } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import "./header.css";
+import { Row, Col } from 'antd';
 
 const userName = JSON.parse(localStorage.getItem("user"));
 
 console.log('LOCAL STOR:' + localStorage)
 
 // const jsUSer = {  "userName": "Ing. Vergara"}
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+        Account Info
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+        Log Out
+      </a>
+    </Menu.Item>
+  </Menu>
+);
+
 const Header = () => {
 
   return (
-    <header>
-      <PageHeader
-        className="main-header"
-        onBack={() => null}
-        title="Palo Alto"
-        subTitle="Especialista en Pisos"
-        extra={[
-          // <Button key="3">Op2</Button>,
-          // <Button key="2">Op1</Button>,
-          <Button key="1" icon={<UserOutlined />}>
-            {userName.nombre}
-          </Button>,
-        ]}
-      />
+    <header className="main-header">
+      <Row>
+      <Col span={6} className="labels-header">PALO ALTO - Especialista en Pisos</Col>
+      <Col span={12}>col-8</Col>
+      <Col span={6}><Dropdown overlay={menu} placement="bottomLeft">
+      <Button icon={<UserOutlined />}>{userName.nombre}</Button>
+      </Dropdown></Col>
+    </Row>
     </header>
   );
 };
