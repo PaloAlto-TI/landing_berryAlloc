05ba46f6f1fbdefddc0e45e4d5207a534b90f8b2
PaloAlto-TI <tecnologiasdelainformacion@paloalto.com.ec>
2021-06-11 09:26:34 -0500

    Merge con master actualizado formatos - AU

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ function App() {
               <Route path="/login">
                 <Login/>
               </Route>
+              <Route  path="/productos">
+               <ProductoContextProvider>
+                <ProductoList />
+                </ProductoContextProvider>
+              </Route> 
 
               {/* <Route  path="/productos">
                 <ProductoContextProvider>
