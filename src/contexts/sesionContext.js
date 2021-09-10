e15ecc2c6ded16e2ced2487b4779707130638492
unknown <jcubero@paloalto.com.ec>
2021-09-10 12:52:29 -0500

    seguridad login mejorada (ahora ya comente revisar use effect de sesionService)

--- src/contexts/sesionContext.js
+++ src/contexts/sesionContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect } from "react";
 import { SesionService } from "../services/sesionService";
-import {  message } from 'antd';
+import { message } from 'antd';
 import { useHistory } from "react-router";
 
 
@@ -19,72 +19,43 @@ const SesionContextProvider = (props) => {
 
 
   useEffect(async () => {
-    
-    // if (productos.length === 0){
-    //   setIsEmpty(true);
-    //console.log("imprime token :"+localStorage.getItem("token"));
-    if (localStorage.getItem("token")) {
-      
+
+    if (localStorage.getItem("token")) {// SE VALIDA SI EXISTE ALGO EN LOCAL STORAGE
+
       const tok = { "token": localStorage.getItem("token") };
       let data = await usuario(tok);
-      console.log("prueba data "+data);
-      
-      
-      console.log("prueba sesions "+JSON.stringify(sesions));
-      //console.log("Entra en effect service1 " + JSON.stringify(sesions));
 
-      if (sesions==="CONSULTA" || !sesions) {
-        if(data){
+
+      if (!sesions) {// VALIDA QUE LA VARIABLE SESIONS ESTE VACIA
+        if (data) { //DATA ES EL RESULTADO DE BUSCAR EL TOKEN EN EL REGISTRO DE SESIONES
           setMoved(true);
-          console.log("prueba data "+JSON.stringify(sesions));
-        setSesions(data);
-        setIsLogged(true);
-        //console.log("fecha ahora:"+new Date()+"milisegundos ahora"+ new Date().getTime())
-        //console.log("fecha salir:"+new Date( (new Date(data.fecha).getTime() + data.time_out))+"milisegundos para salir"+  (new Date(data.fecha).getTime() + data.time_out))
-        
-        if (  new Date().getTime() > (new Date(data.fecha).getTime() + data.time_out) )
-        { 
-          LogOut();
-          setIsLogged(false);
+          console.log("prueba data " + JSON.stringify(sesions));
+          setSesions(data);
 
-          
+          setIsLogged(true);
+          console.log("prueba data " + data);
           
 
-
-
+          if (new Date().getTime() > (new Date(data.fecha).getTime() + data.time_out)) {
+            LogOut();
+            setIsLogged(false);
+          }
+        }
+        else {// SI NO ENCONTRO NADA EN LA BUSQUEDA DEL TOKEN
+          localStorage.clear() //PRIMERO ELIMINA LOCAL STORAGE (LOCAL STORAGE NO DISPARA EL RENDERIZADO CONDICIONAL Y TAMPOCO EL USEEFFECT POR ESTO ES NECESARIO SETTEAR SESIONS(O HACER ALGUN CAMBIO EN ALGUNA VARIABLE DEL ARRAY DE CONTROL DEL USEEFFECT) PARA QUE DISPARE EL RENDERIZADO CONDICIONAL Y POR CONSIGUIENTE EL USE EFFECT)
+          setSesions(null); // ESTA FUNCION DISPARA ESTE USE EFFECT OTRA VEZ PARA QUE EN HOME ENTRE A LA COMPARACION DE LOCAL STORAGE Y REDIRECCIONE A LOGIN
         }
       }
-      else{
-        
-        setSesions(null);
-        localStorage.clear()
-      
-      }
-
-
-
-      }
-     // else{if(!sesions._usuario[0]){window.location.reload()}}
-
-
-
-
-     // console.log("Entra en effect service2 " + new Date(JSON.stringify(data.fecha)));
-      //console.log("Entra en effect service3 " + new Date(new Date(data.fecha).getTime() + data.time_out));
     }
     else {
       setIsLogged(true);
-
     }
-    // }
-
-  //},[localStorage.getItem("token"),sesions],ismoved);
-},[localStorage,sesions,ismoved,setSesions,isLogged]);
+  }, [sesions, ismoved, setSesions, isLogged]);
 
   //----------------------------------------------------------------
 
-  
-  
+
+
   const LogOut = async () => {
     //sesionService.softDelete(sesion);
 
@@ -93,17 +64,17 @@ const SesionContextProvider = (props) => {
 
     await softDeleteSesion(data);
     localStorage.clear();
-   
+
     message.success('Log Out');
     setSesions();
     setMoved(false);
     //history.push("/login");
-  // console.log("cookies:"+ document.cookie.split(";"));
-   //document.cookie = "userId=nick123";
+    // console.log("cookies:"+ document.cookie.split(";"));
+    //document.cookie = "userId=nick123";
 
-    
+
     //removeCookies();
-   
+
     //setSesions(data);
     // console.log("Entra en sesion");
     // setSesiones([...sesiones, data.data]);
@@ -145,7 +116,7 @@ const SesionContextProvider = (props) => {
         sesions,
         LogOut,
         setMoved
-        
+
       }}
     >
       {props.children}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+
+
 ReactDOM.render(
+  
     <App />,
   document.getElementById('root')
 );
