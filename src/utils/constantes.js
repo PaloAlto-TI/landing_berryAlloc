658e073e8e2d770c9d8c3344eaff64d68c1cb0a7
unknown <jcubero@paloalto.com.ec>
2021-09-28 11:06:22 -0500

    stocks servidor como master v2 28/09/2021

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -2,4 +2,8 @@
 //export const baseUrl="http://localhost:5050/";
 //export const baseUrl="https://api-palo-alto.herokuapp.com/";
 //export const baseUrl="http://localhost:5050/";
+
+//nsd fm,sdn g.sdng,dfng.,fdnb
+
+
 export const baseUrl="https://api-palo-alto.herokuapp.com/";
