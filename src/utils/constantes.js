e455168e6cd6f4ba1265918480ec3d0039020ce8
mcoronel2021pa <mcoronel@paloalto.com.ec>
2021-10-01 15:01:39 -0500

    ARREGLO DE LLAMADO DE API A CONTIFICO EN PRODUCTOS STOCKS, AHORA SE HACE A TRAVÉS DEL CAMPO: CODIGO_INTERNO- MC

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,9 +1,9 @@
 
 //export const baseUrl="http://localhost:5050/";
 //export const baseUrl="https://api-palo-alto.herokuapp.com/";
-export const baseUrl="http://localhost:5050/";
+// export const baseUrl="http://localhost:5050/";
 
 //nsd fm,sdn g.sdng,dfng.,fdnb
 
 
-// export const baseUrl="https://api-palo-alto.herokuapp.com/";
+export const baseUrl="https://api-palo-alto.herokuapp.com/";
