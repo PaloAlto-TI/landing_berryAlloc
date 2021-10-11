2c7b9399132fdf4e694f94cfffadf591b6dc330e
unknown <jcubero@paloalto.com.ec>
2021-10-11 07:58:06 -0500

    produccion manuel coronel v1 11/10/2021

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,19 +1,11 @@
 
 //export const baseUrl="http://localhost:5050/";
 //export const baseUrl="https://api-palo-alto.herokuapp.com/";
-<<<<<<< HEAD
 // export const baseUrl="http://localhost:5050/";
-=======
-export const baseUrl="http://localhost:5050/";
->>>>>>> PRODUCTOS_PA_MC
 
 //nsd fm,sdn g.sdng,dfng.,fdnb
 
 
-<<<<<<< HEAD
 export const baseUrl="https://api-palo-alto.herokuapp.com/";
 
 //ksdmngflksdnñlgkmñflmghñsldfmhlsñfmhñlkfnhñsln
-=======
-// export const baseUrl="https://api-palo-alto.herokuapp.com/";
->>>>>>> PRODUCTOS_PA_MC
